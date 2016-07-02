102082d81e25b15c1cd76e2ca5f7399323ed9fbf
ildar <ildar@localhost.localdomain>
2016-07-02 12:19:52 +0300

    eee, std.cpp

---
+++ std.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fc first
+#define sc second
+#define endl '\n'
+#define ll long long
+#define forn(i, n) for(int i = 0; i < (int) (n); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main() {
+    ios_base::sync_with_stdio(0);
+}
